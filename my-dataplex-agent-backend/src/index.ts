cd7b86d52b17e4f13b462b618d8239567bf33981
pankaj.deshmukh1 <pankaj.deshmukh1@wipro.com>
2026-01-05 11:38:35 +0000

    Added missing BQ and GCS capabilities4

--- my-dataplex-agent-backend/src/index.ts
+++ my-dataplex-agent-backend/src/index.ts
@@ -76,10 +76,10 @@ async function startServer() {
             *   **Run**: dataplex datascans run my-scan-id --project=my-project-id --location=us-central1
             *   **Delete**: dataplex datascans delete my-scan-id --project=my-project-id --location=us-central1
             *   **Updating Rules**: To add, update, or remove rules, you MUST use the \`dataplex datascans update\` command. You need to provide the *entire* \`dataQualitySpec\` object in the body of the command and specify \`"dataQualitySpec"\` in the \`--update-mask\`.
-                *   **Example**: \`dataplex datascans update my-scan-id --project=my-project-id --location=us-central1 --update-mask="dataQualitySpec" --body='{"dataQualitySpec": {"rules": [{"column": "email", "dimension": "VALIDITY", "nonNullExpectation": {}}, {"column": "country", "dimension": "VALIDITY", "setExpectation": {"values": ["US", "GB", "CA"]}}]}}'\`
+                *   **Example**: \`dataplex datascans update my-scan-id --project=my-project-id --location=us-central1 --update-mask="dataQualitySpec" --body='{{"dataQualitySpec": {{"rules": [{{"column": "email", "dimension": "VALIDITY", "nonNullExpectation": {{}}}}, {{\"column\": "country", "dimension": "VALIDITY", "setExpectation": {{\"values": ["US", "GB", "CA"]}}}}]}}}}\'\`
 
         *   **Dataplex - Data Profiling Scans**
-            *   **Run**: \`dataplex datascans create --project=my-project-id --location=us-central1 --body='{ "data_profile_spec": {}, "data": { "resource": "//bigquery.googleapis.com/projects/my-project-id/datasets/my-dataset/tables/my-table" } }\'\`
+            *   **Run**: \`dataplex datascans create --project=my-project-id --location=us-central1 --body='{{ "data_profile_spec": {{}}, "data": {{ "resource": "//bigquery.googleapis.com/projects/my-project-id/datasets/my-dataset/tables/my-table" }} }}\'\`
 
         *   **BigQuery - Show Table Schema**: bq show --schema --format=prettyjson my-project-id:my_dataset.my_table
         *   **Cloud Storage - List Buckets**: gsutil ls --project my-project-id
@@ -91,14 +91,14 @@ async function startServer() {
 
         *   **Rule Structure Example**: A rule will look like this. You should be able to explain what each part means.
             \`\`\`json
-            {
+            {{
               "column": "city",
               "dimension": "VALIDITY",
               "ruleType": "SET_EXPECTATION",
-              "setExpectation": {
+              "setExpectation": {{
                 "values": ["New York", "London", "Tokyo"]
-              }
-            }
+              }}
+            }}
             \`\`\`
     `;
 
